02d446f4535a7d1016977d5e1aed0d55ff44bb9b
jashanbanga <bangajashan17@gmail.com>
2019-01-02 18:13:54 +0530

    Created new model

--- backend/BEProjectsApp/models.py
+++ backend/BEProjectsApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class Teacher(models.Model):
+	teacher_id=models.AutoField(primary_key=True)
+	teacher_name=models.CharField(max_length=25)
+	username=models.CharField(max_length=25)
+	password=models.CharField(max_length=256)
+	subject=models.CharField(max_length=25)
 # Create your models here.
