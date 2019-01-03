0db3b52257030e00c7b22b8b9fa8f54df88d7aa9
jashanbanga <bangajashan17@gmail.com>
2019-01-03 18:40:47 +0530

    Made changes to the model

--- BEProjectsApp/models.py
+++ BEProjectsApp/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class Teacher(models.Model):
     teacher_id = models.AutoField(primary_key=True)
-    teacher_name = models.CharField(max_length=25)
-    username = models.CharField(max_length=25)
+    teacher_name = models.CharField(max_length=150)
+    username = models.CharField(max_length=150)
     password = models.CharField(max_length=256)
-    subject = models.CharField(max_length=25)
+    subject = models.CharField(max_length=150)
 
     def __str__(self):
         return self.teacher_name
