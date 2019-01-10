036c27b46645bef89e8465aa7d014348a3b20e6a
Rashmil Panchani <32737711+Rashmil-1999@users.noreply.github.com>
2019-01-10 21:45:44 +0530

    freshmen

--- BEProjectsApp/admin.py
+++ BEProjectsApp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Teacher, Inhouse_Project
+from .models import Teacher, Inhouse_Project,OH_Project
 
 # Register your models here.
 admin.site.register(Teacher)
 admin.site.register(Inhouse_Project)
+admin.site.register(OH_Project)
--- BEProjectsApp/models.py
+++ BEProjectsApp/models.py
@@ -1,6 +1,24 @@
 from django.db import models
 from django.utils import timezone
 
+DOMAIN_OHP = [
+    'Data Mining & Analytics',
+    'Machine Learning',
+    'Image Processing/Computer Vision',
+    'Natural Language Processing/Artificial Intelligence',
+    'Networking/Security',
+    'Internet of Things(IOT)',
+    'Mobile Computing',
+    'Big Data',
+    'Cloud Computing',
+    'Computer Vision & Artificial Intelligence'
+    ]
+
+//convert into tuple
+DOMAIN_CHOICES_OHP = sorted((item,item) for item in DOMAIN_OHP)
+
+//add other field
+DOMAIN_CHOICES_OHP.append(('other','other'))
 
 DOMAIN_CHOICES = [
     (1, ("Data Mining & Analytics")),
@@ -80,3 +98,49 @@ class Inhouse_Project(models.Model):
 
     def __str__(self):
         return self.title
+
+class OH_Project(models.Model):
+
+    #Store the Project Title
+    name = models.CharField(max_length = 100, blank = False, default = 'Untitled Project')
+    #Store the ID of the Teacher model who is mentoring the project
+    teacher_id = models.ForeignKey(
+        Teacher, related_name = 'teachers', on_delete = models.CASCADE
+    )
+    #project description
+    description = models.TextField()
+    #year published and created will be stored
+    year_created = models.DateTimeField(default = timezone.now)
+    year_published = models.DateTimeField(blank = True, null = True)
+    #to store the files uploaded
+    document = models.FileField()
+    #domain list
+    domain = models.CharField(
+        choices = DOMAIN_CHOICES_OHP, default = 'none', blank = False, max_length = 100
+    )
+    #out house details
+    company = models.CharField(max_length = 100, blank = False, default = 'none')
+    supervisor = models.CharField(max_length = 100, blank = False, default = 'none')
+    #contributers stored as a text field
+    contributers = models.TextField()
+
+    '''
+
+    The purpose of the pubish function is to create the functionality of drafts where
+
+    the teachers get to approve a project and then it will be published till then
+
+    only created date is stored.
+
+    The display of search results will only be done with the published date so if that
+
+    is empty it should not be shown in the search results.
+
+    '''
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.name
