7cd74222e1e3da598b1c1d38796272921f13ceda
Rashmil Panchani <32737711+Rashmil-1999@users.noreply.github.com>
2019-01-11 17:48:20 +0530

    black done successfully!

--- BEProjectsApp/admin.py
+++ BEProjectsApp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Teacher, Inhouse_Project,OH_Project
+from .models import Teacher, Inhouse_Project, OH_Project
 
 # Register your models here.
 admin.site.register(Teacher)
--- BEProjectsWeb/settings.py
+++ BEProjectsWeb/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+MEDIA_DIR = os.path.join(BASE_DIR, "media")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -114,5 +114,5 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-MEDIA_URL = '/media/'
+MEDIA_URL = "/media/"
 MEDIA_ROOT = MEDIA_DIR
