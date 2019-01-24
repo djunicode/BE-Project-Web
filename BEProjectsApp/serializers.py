97d20c6f31813e7d696b3fb08de52c578b459863
Nirav <niravjain1709@gmail.com>
2019-01-24 21:03:34 +0530

    Adds media root in project level urls.py

--- BEProjectsApp/serializers.py
+++ BEProjectsApp/serializers.py
@@ -2,18 +2,18 @@ from .models import Teacher, Inhouse_Project,Outhouse_Project
 from rest_framework import serializers
 
 class InhouseProjectSerializer(serializers.HyperlinkedModelSerializer):
-    teacher_id = serializers.ReadOnlyField(source = 'teacher_id.username')
+    teacher = serializers.ReadOnlyField(source = 'teacher_id.teacher_name')
     
     class Meta:
         model = Inhouse_Project
-        fields = ('title','teacher_id','description','year_created','year_published','document','approved','contributers','domain')
+        fields = ('title','teacher','description','year_created','year_published','document','approved','contributers','domain')
 
 class OuthouseProjectSerializer(serializers.HyperlinkedModelSerializer):
-    teacher_id = serializers.ReadOnlyField(source = 'teacher_id.username')
+    teacher = serializers.ReadOnlyField(source = 'teacher_id.teacher_name')
     
     class Meta:
         model = Outhouse_Project
-        fields = ('title','teacher_id','description','year_created','document','approved','contributers','domain','company','supervisor')
+        fields = ('title','teacher','description','year_created','document','approved','contributers','domain','company','supervisor')
 
 class TeacherSerializer(serializers.HyperlinkedModelSerializer):
     inhouse_projects = serializers.HyperlinkedRelatedField(many = True, view_name = 'BEProjectsApp:inhouse_project-detail', read_only = True)
--- BEProjectsWeb/urls.py
+++ BEProjectsWeb/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import include, url
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("",include('BEProjectsApp.urls'))
     ]
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
