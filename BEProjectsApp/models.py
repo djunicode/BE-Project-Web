34e942f991000040e67220ed14dbe0b2094ec40c
jashanbanga <bangajashan17@gmail.com>
2019-02-28 18:57:54 +0530

    Teacher model extends User Model

--- BEProjectsApp/models.py
+++ BEProjectsApp/models.py
@@ -1,12 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+DOMAIN_CHOICES = [
+    (1, ("Data Mining & Analytics")),
+    (2, ("Machine Learning")),
+    (3, ("Deep Learning")),
+    (4, ("Image Processing/Computer Vision")),
+    (5, ("Natural Language Processing/Artificial Intelligence")),
+    (6, ("Networking/Security")),
+    (7, ("Internet of Things(IOT)")),
+    (8, ("Mobile Computing")),
+    (9, ("Big Data")),
+    (10, ("Cloud Computing")),
+    (11, ("Computer Vision & Artificial Intelligence")),
+    (12, ("Blockchain")),
+]
 
 
 class Teacher(models.Model):
-    teacher_id = models.AutoField(primary_key=True)
-    teacher_name = models.CharField(max_length=150)
-    username = models.CharField(max_length=150)
-    password = models.CharField(max_length=256)
-    subject = models.CharField(max_length=150)
+    user = models.OneToOneField(User, on_delete="models.CASCADE")
+    domain = models.IntegerField(choices=DOMAIN_CHOICES, default="none", blank=False)
 
     def __str__(self):
-        return self.teacher_name
+        return self.user.first_name + " " + self.user.last_name
