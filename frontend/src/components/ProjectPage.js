d73c90df8285a94ef0c3b7d6dd23eca6dfcf7366
jashmehta3300 <jashmehta3300@gmail.com>
2020-07-26 13:36:40 +0530

    changes in project desc page

--- frontend/src/components/ProjectPage.js
+++ frontend/src/components/ProjectPage.js
@@ -10,7 +10,7 @@ import { Grid, IconButton } from '@material-ui/core';
 import DescriptionIcon from '@material-ui/icons/Description';
 
 const ProjectCardDes_modal = styled.div`
-  font-size: 1.4em;
+  font-size: 1.2em;
   color: #578bd8;
   font-weight: 600;
 `;
@@ -22,105 +22,142 @@ const ProjectCardDetail_modal = styled.div`
 
 export default function ProjectPage(props) {
     const [project, setProject] = React.useState(props.project);
+    const [teacher, setTeacher] = React.useState(false);
 
     return (
       <React.Fragment>
-          <Dialog
-            maxWidth={'lg'}
-            open={true}
-            onClose={props.closeFn}
-            aria-labelledby="alert-dialog-title"
-            aria-describedby="alert-dialog-description"
-          >
-            <DialogTitle id="alert-dialog-title">
-              <h4
-                style={{
-                  textAlign: 'center',
-                  color: '#578bd8',
-                  fontSize: '35px',
-                  fontWeight: '600',
-                }}
-              >
-                {'Project Details'}
-              </h4>
-            </DialogTitle>
-            <DialogContent>
-              <Grid container style={{ paddingLeft: '2vh', marginTop: '5vh' }}>
-                <Grid item md={9} xs={9}>
-                  <ProjectCardDes_modal>Project Name</ProjectCardDes_modal>
-                  <h4>{project.title}</h4>
-                </Grid>
-                <Grid item md={3} xs={3}>
-                  <IconButton
-                    target="_blank"
-                    variant="contained"
-                    color="primary"
-                    href={project.document}
-                  >
-                    <DescriptionIcon />
-                  </IconButton>
-                </Grid>
+        {console.log(props.screen)}
+        <Dialog
+          maxWidth={'lg'}
+          open={true}
+          fullScreen={props.screen}
+          onClose={props.closeFn}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+            <h4
+              style={{
+                textAlign: 'center',
+                color: '#578bd8',
+                fontSize: '40px',
+                fontWeight: '600',
+              }}
+            >
+              {'Project Details'}
+            </h4>
+          </DialogTitle>
+          <DialogContent>
+            <Grid container style={{ paddingLeft: '2vh', marginTop: '5vh' }}>
+              <Grid item md={12} xs={12}>
+                <ProjectCardDes_modal>Project Name</ProjectCardDes_modal>
+                <h4>{project.title}</h4>
               </Grid>
-              <Grid container style={{ paddingLeft: '2vh', marginTop: '5vh' }}>
-                <Grid item md={12} xs={12}>
-                  <ProjectCardDes_modal>Description</ProjectCardDes_modal>
-                  <ProjectCardDetail_modal>
-                    {dummy_text}
-                    {dummy_text}
-                    {dummy_text}
-                    {dummy_text}
-                    {dummy_text}
-                    {dummy_text}
-                  </ProjectCardDetail_modal>
-                </Grid>
+            </Grid>
+            <Grid
+              container
+              style={{
+                paddingLeft: '2vh',
+                marginTop: '5vh',
+                marginBottom: '5vh',
+              }}
+            >
+              <Grid item md={4} xs={12} style={{ marginTop: '20px' }}>
+                <ProjectCardDes_modal>Domain</ProjectCardDes_modal>
+                <ProjectCardDetail_modal>
+                  {project.domain}
+                </ProjectCardDetail_modal>
               </Grid>
-              <Grid
-                container
-                style={{
-                  paddingLeft: '2vh',
-                  marginTop: '5vh',
-                  marginBottom: '5vh',
-                }}
-              >
-                <Grid item md={3} xs={12}>
-                  <ProjectCardDes_modal>Domain</ProjectCardDes_modal>
-                  <ProjectCardDetail_modal>
-                    {' '}
-                    {project.domain}{' '}
-                  </ProjectCardDetail_modal>
-                </Grid>
-                <Grid item md={3} xs={12}>
-                  <ProjectCardDes_modal>Guide</ProjectCardDes_modal>
-                  <ProjectCardDetail_modal> Teacher</ProjectCardDetail_modal>
-                </Grid>
-                <Grid item md={3} xs={12}>
-                  <ProjectCardDes_modal>Year</ProjectCardDes_modal>
-                  <ProjectCardDetail_modal>
-                    {' '}
-                    {project.year_created}{' '}
-                  </ProjectCardDetail_modal>
-                </Grid>
-                <Grid item md={3} xs={12}>
-                  <ProjectCardDes_modal>Group Members</ProjectCardDes_modal>
-                  <ProjectCardDetail_modal>
-                    {[0].map((dummy) => {
-                      let cbs = '';
-                      project.contributor.forEach((contri) => {
-                        cbs += `${contri.name},`;
-                      });
-                      cbs = cbs.slice(0, -1);
-                      return cbs;
-                    })}
-                  </ProjectCardDetail_modal>
-                </Grid>
+              <Grid item md={4} xs={12} style={{ marginTop: '20px' }}>
+                <ProjectCardDes_modal>Guide</ProjectCardDes_modal>
+                <ProjectCardDetail_modal> Teacher</ProjectCardDetail_modal>
               </Grid>
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={props.closeFn} color="primary" autoFocus>
-                CLOSE
-              </Button>
-            </DialogActions>
-          </Dialog>
+              <Grid item md={4} xs={12} style={{ marginTop: '20px' }}>
+                <ProjectCardDes_modal>Year</ProjectCardDes_modal>
+                <ProjectCardDetail_modal>
+                  {project.year_created}
+                </ProjectCardDetail_modal>
+              </Grid>
+            </Grid>
+            <Grid container style={{ paddingLeft: '2vh', marginTop: '5vh' }}>
+              <Grid item md={12} xs={12}>
+                <ProjectCardDes_modal>Abstract</ProjectCardDes_modal>
+                <ProjectCardDetail_modal>
+                  {dummy_text}
+                  {dummy_text}
+                </ProjectCardDetail_modal>
+                <h7
+                  style={{
+                    textAlign: 'center',
+                    color: '#578bd8',
+                    fontSize: '2vh',
+                  }}
+                >
+                  {
+                    'Please contact the contributor or teacher for more details...'
+                  }
+                </h7>
+              </Grid>
+            </Grid>
+            <Grid
+              container
+              style={{
+                paddingLeft: '2vh',
+                marginTop: '5vh',
+                marginBottom: '5vh',
+              }}
+            >
+              <Grid item md={12} xs={12}>
+                <ProjectCardDes_modal>Contributors</ProjectCardDes_modal>
+                <ProjectCardDetail_modal>
+                  {[0].map((dummy) => {
+                    let cbs = '';
+                    project.contributor.forEach((contri) => {
+                      cbs += `${contri.name},`;
+                    });
+                    cbs = cbs.slice(0, -1);
+                    return cbs;
+                  })}
+                </ProjectCardDetail_modal>
+              </Grid>
+            </Grid>
+            <Grid
+              container
+              style={{
+                paddingLeft: '2vh',
+                marginTop: '5vh',
+                marginBottom: '5vh',
+              }}
+            >
+              <Grid item md={12} xs={12}>
+                <ProjectCardDes_modal>Journal Publication</ProjectCardDes_modal>
+                <ProjectCardDetail_modal>
+                  NA
+                </ProjectCardDetail_modal>
+              </Grid>
+            </Grid>
+            <Grid
+              container
+              style={{
+                paddingLeft: '2vh',
+                marginTop: '5vh',
+                marginBottom: '5vh',
+              }}
+            >
+              <Grid item md={12} xs={12}>
+                <ProjectCardDes_modal>Winning Project</ProjectCardDes_modal>
+                <ProjectCardDetail_modal>
+                  NA
+                </ProjectCardDetail_modal>
+              </Grid>
+            </Grid>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={props.closeFn} color="primary" autoFocus>
+              CLOSE
+            </Button>
+          </DialogActions>
+        </Dialog>
       </React.Fragment>
     );
 }
